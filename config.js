e59789aff5aa9bd90a4ba5321aa44112c5883389
kunalkw <126095927+kunalkw312@users.noreply.github.com>
2026-02-16 01:01:01 +0530

    Update GEMINI_API_KEY in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 const CONFIG = {
-    GEMINI_API_KEY: "AIzaSy..." // Paste your real key here
+    GEMINI_API_KEY: "AIzaSyDSdQjJ2uE-6HgBeI5nIMCmeJhPBX4m6Mc" // Paste your real key here
 };
